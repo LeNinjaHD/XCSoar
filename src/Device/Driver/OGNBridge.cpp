f2c3bf22dd5916bffb61e18be76c7f84881e94d5
LeNinjaHD <58134535+LeNinjaHD@users.noreply.github.com>
2024-11-11 19:45:20 +0100

    Added OGN Bridge Driver

---
+++ src/Device/Driver/OGNBridge.cpp
@@ -0,0 +1,18 @@
+#include "Device/Driver/OGNBridge.hpp"
+#include "Device/Driver.hpp"
+
+class OGNBridgeDevice : public AbstractDevice {
+};
+
+static Device *
+OGNBridgeCreateOnPort([[maybe_unused]] const DeviceConfig &config, [[maybe_unused]] Port &com_port)
+{
+  return new OGNBridgeDevice();
+}
+
+const struct DeviceRegister OGNBridge_driver = {
+  _T("OGNBridge"),
+  _T("OGNBridge"),
+  0,
+  OGNBridgeCreateOnPort,
+};
